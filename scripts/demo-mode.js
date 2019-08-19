50d866fdf804b77bad8fad8291ab051820e18450
aricallen <aric.allen2@gmail.com>
2019-08-18 17:01:56 -0700

    update controller engine

--- scripts/demo-mode.js
+++ scripts/demo-mode.js
@@ -1,6 +1,15 @@
 const NoteEngine = require('../lib/note-engine');
 const RootManager = require('../lib/root-manager');
-const { ADDRESS_BASS, ADDRESS_PADS, ADDRESS_SPARKLES } = require('../lib/constants');
+const {
+  ADDRESS_BASS,
+  ADDRESS_PADS,
+  ADDRESS_SPARKLES,
+  ADDRESS_ALPHA,
+  ADDRESS_BETA,
+  ADDRESS_THETA,
+  ADDRESS_GAMMA,
+} = require('../lib/constants');
+const ControllerEngine = require('../lib/controller-engine');
 
 const rootManager = new RootManager();
 
@@ -39,7 +48,32 @@ const sparklesEngine = new NoteEngine({
   notesAtATime: 6,
 });
 
-const engines = [bassEngine, padsEngine, sparklesEngine];
+const alphaEngine = new ControllerEngine({
+  address: ADDRESS_ALPHA,
+  channel: 0,
+  controllerNumber: 12,
+});
+
+const betaEngine = new ControllerEngine({
+  address: ADDRESS_BETA,
+  channel: 1,
+  controllerNumber: 13,
+});
+
+const thetaEngine = new ControllerEngine({
+  address: ADDRESS_THETA,
+  channel: 2,
+  controllerNumber: 14,
+});
+
+const gammaEngine = new ControllerEngine({
+  address: ADDRESS_ALPHA,
+  channel: 3,
+  controllerNumber: 15,
+});
+
+const waveEngines = [alphaEngine, betaEngine, thetaEngine, gammaEngine];
+const engines = [bassEngine, padsEngine, sparklesEngine, ...waveEngines];
 
 const valueMap = new Map();
 valueMap.set(bassEngine.address, Math.random());
@@ -49,17 +83,29 @@ valueMap.set(sparklesEngine.address, Math.random());
 const VAL_INCR = 0.05;
 const RUN_INTERVAL = 1000;
 
+const getNewVal = (val) => {
+  if (val >= 1) {
+    return val - 0.75;
+  }
+
+  if (val <= 0) {
+    return val + 0.25;
+  }
+
+  return Math.random() > 0.5 ? val + VAL_INCR : val - VAL_INCR;
+};
+
 const updateValues = () => {
   engines.forEach((engine) => {
     const currVal = valueMap.get(engine.address);
-    const newVal = Math.random() > 0.5 ? currVal + VAL_INCR : currVal - VAL_INCR;
+    const newVal = getNewVal(currVal);
     valueMap.set(engine.address, newVal);
   });
 };
 
 const run = () => {
   updateValues();
-  // send to sound engines latest value
+  // send to engines latest value
   engines.forEach((engine) => {
     engine.update(valueMap.get(engine.address));
   });
