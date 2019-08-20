6f78b378cf279ef7546967abeb0681020540e5c7
aricallen <aric.allen2@gmail.com>
2019-08-20 12:06:55 -0700

    add viz comps

---
+++ viz/canvas-engine.js
@@ -0,0 +1,69 @@
+/**
+ * create 4 quadrants
+ * draw 4 dots in center of each quadrant
+ * get each to move across screen
+ * move new dots to a value of quad.height * val (between -2 & 2)
+ */
+
+const { ADDRESS_ALPHA, ADDRESS_BETA, ADDRESS_GAMMA, ADDRESS_THETA } = require('../lib/constants');
+
+const canvas = document.getElementById('canvas');
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+canvas.style.width = `${windowWidth}px`;
+canvas.style.height = `${windowHeight}px`;
+
+const center = {
+  x: windowWidth / 2,
+  y: windowHeight / 2,
+};
+
+const waves = [ADDRESS_ALPHA, ADDRESS_BETA, ADDRESS_THETA, ADDRESS_GAMMA];
+const colors = ['blue', 'red', 'orange', 'purple'];
+
+const quads = [0, 1, 2, 3].map((i) => {
+  const quadHeight = windowHeight / waves.length;
+  return {
+    wave: waves[i],
+    origin: {
+      x: 0,
+      y: quadHeight * i,
+    },
+    entryPoint: {
+      x: center.x,
+      y: i === 0 ? quadHeight / 2 : quadHeight / 2 + quadHeight * i,
+    },
+    size: {
+      w: windowWidth,
+      y: quadHeight,
+    },
+    color: colors[i],
+  };
+});
+
+const ctx = canvas.getContext('2d');
+ctx.fillRect(0, 0, windowWidth, windowHeight);
+
+const dotSize = 4;
+
+const drawDot = (quad) => {
+  ctx.save();
+  ctx.fillStyle = quad.color;
+  ctx.translate(quad.entryPoint.x - dotSize, quad.entryPoint.y - dotSize);
+  ctx.fillRect(0, 0, dotSize, dotSize);
+  ctx.restore();
+};
+
+const shift = () => {
+  const prevFrame = ctx.getImageData(dotSize, 0, ctx.canvas.width / 2, ctx.canvas.height);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, windowWidth, windowHeight);
+  ctx.putImageData(prevFrame, 0, 0);
+};
+
+setInterval(() => {
+  quads.forEach(drawDot);
+  shift();
+}, 500);
